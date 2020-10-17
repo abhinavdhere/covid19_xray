eecde2f4f602fcd636792d36f514bb1cea874e54
Abhinav Dhere <abhinavdhere@gmail.com>
2020-10-18 00:11:57 +0530

    Minor clean up

--- model.py
+++ model.py
@@ -62,62 +62,6 @@ class ResBlock(nn.Module):
         res = F.relu(res)
         return res, out
 
-# class ResNet(nn.Module):
-#     def __init__(self, in_channels, num_blocks, num_layers, num_classes=2,
-#                  num_feats=64, downsample_freq=1):
-#         '''
-#         Args:
-#             in_channels (int): number of channels in input
-#             num_blocks (int): number of residual block
-#             num_layers (int): number of layers in each block
-#             num_classes (int): number of classes in classification
-#             num_feats (int): number of feature channels in first layer
-#             downsample_freq (int): number of blocks after which to downsample.
-#         '''
-#         super(ResNet, self).__init__()
-#         self.downsample_freq = downsample_freq
-#         self.conv1 = nn.Conv2d(in_channels, num_feats, kernel_size=7,
-#                                stride=2, padding=3, bias=False)
-#         self.bn1 = nn.BatchNorm2d(num_feats)
-#         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-#         self.layers = []
-#         init_feats = num_feats
-#         downsample = False
-#         for block_num in range(num_blocks):
-#             block = ResBlock(init_feats, num_feats, num_layers,
-#                              downsample=downsample, attention=True)
-#             init_feats = num_feats
-#             self.layers.append(block.cuda())
-#             downsample = False
-#             cond = (block_num + 1 != num_blocks)
-#             if ((block_num+1) % self.downsample_freq == 0) and cond:
-#                 num_feats *= 2
-#                 downsample = True
-#         self.main_arch = nn.Sequential(*self.layers)
-#         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-#         self.final = nn.Linear(num_feats, num_classes)
-#         self.weights = nn.Parameter(torch.ones(num_blocks, 128, 128)).cuda()
-
-#     def forward(self, x):
-#         attn_map_list = []
-#         x = F.relu(self.bn1(self.conv1(x)))
-#         x = self.maxpool(x)
-#         # import pdb ; pdb.set_trace()
-#         for block in self.main_arch:
-#             x, attn_map = block(x)
-#             attn_map = F.interpolate(attn_map, (128, 128),
-#                                      align_corners=False, mode='bilinear')
-#             attn_map, _ = torch.max(attn_map, 1)
-#             attn_map_list.append(attn_map)
-#         # x, attn_map = self.main_arch(x)
-#         x = self.avgpool(x)
-#         x = torch.flatten(x, 1)
-#         out = self.final(x)
-#         ms_attn = (attn_map_list[0]*self.weights[0]
-#                    + attn_map_list[1]*self.weights[1]
-#                    + attn_map_list[2]*self.weights[2]
-#                    + attn_map_list[3]*self.weights[3])
-#         return out
 class ResNet(nn.Module):
     def __init__(self, in_channels, num_blocks, num_layers, num_classes=2,
                  num_feats=64, downsample_freq=1):
