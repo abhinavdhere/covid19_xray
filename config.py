a8d3922e080ae0c306591f5deae38ac1af07f01d
Abhinav <abhinav.dhere@research.iiit.ac.in>
2020-10-18 00:21:05 +0530

    Minor clean up

--- config.py
+++ config.py
@@ -1,22 +1,23 @@
 from collections import namedtuple
 
-path = '/home/abhinav/kits_2d'
+path = '/home/abhinav/CXR_datasets/RSNA_dataset/my_splits'
 Metrics = namedtuple('Metrics', ['Loss', 'Acc', 'F1', 'AUROC', 'AUPRC',
                                  'fpr_tpr_arr', 'precision_recall_arr'])
-dataset_list = ['kits']
+dataset_list = ['rsna']
 imgDims = (512, 512)
 
 window = (-79, 200)
-crop_params = (210, 215)
+# crop_params = (210, 215)
+crop_params = (208, 224)
 
 # # Used variables
 # window = (-79, 304)
+# path = '/home/abhinav/kits_2d'
 # path = '/home/abhinav/covid19_data_xray/'
 # path = '/home/abhinav/CXR_datasets/COVIDx_data'
-# path = '/home/abhinav/CXR_datasets/RSNA_dataset/my_splits'
 # path = '/home/abhinav/cats_and_dogs'    # dataset path
 
 # dataSetList = ['chxs', 'kg', 'padch']
-# dataSetList = ['rsna']
+# dataset_list = ['kits']
 # dataSetList = ['catdog']
 # dataSetList = ['actmed', 'fig1', 'sirm', 'rsna']
