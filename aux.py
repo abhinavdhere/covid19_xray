55f2c3655542918a6629ee59175a78bae8312403
Abhinav <abhinav.dhere@research.iiit.ac.in>
2020-10-30 03:16:58 +0530

    code for lung segmentation

--- aux.py
+++ aux.py
@@ -149,6 +149,46 @@ def weightedBCE(weight, pred, target):
     return loss
 
 
+class DiceCoeff():
+    """Dice coeff for individual examples"""
+
+    def forward(self, input, target):
+        self.save_for_backward(input, target)
+        eps = 0.0001
+        self.inter = torch.dot(input.view(-1), target.view(-1))
+        self.union = torch.sum(input) + torch.sum(target) + eps
+
+        t = (2 * self.inter.float() + eps) / self.union.float()
+        return t
+
+    # This function has only a single output, so it gets only one gradient
+    def backward(self, grad_output):
+
+        input, target = self.saved_variables
+        grad_input = grad_target = None
+
+        if self.needs_input_grad[0]:
+            grad_input = grad_output * 2 * (target * self.union - self.inter) \
+                         / (self.union * self.union)
+        if self.needs_input_grad[1]:
+            grad_target = None
+
+        return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+    """Dice coeff for batches"""
+    if input.is_cuda:
+        s = torch.FloatTensor(1).cuda().zero_()
+    else:
+        s = torch.FloatTensor(1).zero_()
+
+    for i, c in enumerate(zip(input, target)):
+        s = s + DiceCoeff().forward(c[0], c[1])
+
+    return s / (i + 1)
+
+
 def toCategorical(yArr):
     '''
     One Hot encoding for softmax
@@ -157,6 +197,17 @@ def toCategorical(yArr):
     y_OH.zero_()
     y_OH.scatter_(1, yArr, 1)
     return y_OH
+
+
+def integralDice(pred, gt, k):
+    '''
+    Dice coefficient for multiclass hard thresholded prediction consisting
+    of integers instead of binary values.
+    k = integer for class for which Dice is being calculated.
+    '''
+    return ((torch.sum(pred[gt == k] == k)*2.0 /
+            (torch.sum(pred[pred == k] == k)
+             + torch.sum(gt[gt == k] == k)).float()))
 
 
 def AUC(soft_predList, labelList):
